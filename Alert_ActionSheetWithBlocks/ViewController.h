4ac5a5ce8ea9f40bf1142b3c475d7d8a01f55b4a
Gurpreet Singh <gurpreet.singh@SmartBuzzs-Mac-Pro.local>
2014-01-06 16:49:24 +0530

    Initial Commit

---
+++ Alert_ActionSheetWithBlocks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alert_ActionSheetWithBlocks
+//
+//  Created by Gurpreet Singh on 06/01/14.
+//  Copyright (c) 2014 Gurpreet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Alert_ActionSheetWithBlocksTests/Alert_ActionSheetWithBlocksTests.m
@@ -0,0 +1,34 @@
+//
+//  Alert_ActionSheetWithBlocksTests.m
+//  Alert_ActionSheetWithBlocksTests
+//
+//  Created by Gurpreet Singh on 06/01/14.
+//  Copyright (c) 2014 Gurpreet Singh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Alert_ActionSheetWithBlocksTests : XCTestCase
+
+@end
+
+@implementation Alert_ActionSheetWithBlocksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
